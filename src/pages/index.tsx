db9c3b27559a4ee8ffee24f308eedf9423c6067a
Marcelo Martins <m.a.martins@live.com>
2021-02-24 22:56:06 -0300

    Created structure and styles of ChallengeBox component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
+import Head from "next/head";
 import styles from '../styles/pages/Home.module.css';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { ChallengeBox } from "../components/ChallengeBox";
 import { Countdown } from "../components/Countdown";
-
-import Head from "next/head";
 
 export default function Home() {
   return (
@@ -22,7 +22,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
